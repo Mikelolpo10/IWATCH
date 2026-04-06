aef9e394dc7723f0fc4c1acbd4be6fd5b867cf79
Mikelolpo10 <xzvmike@gmail.com>
2026-04-06 17:42:56 +0700

    Add a second return if run time less than 1 hour

--- src/utils/formatRunTime.js
+++ src/utils/formatRunTime.js
@@ -1,7 +1,12 @@
 export default function formatRuntime(min) {
   const h = Math.floor(min / 60);
   const m = min % 60;
-  return `${h}h ${m}min`;
+
+  if (h !== 0) {
+    return `${h}h ${m}min`;
+  } else {
+    return `${m}min`;
+  }
 }
 
 
